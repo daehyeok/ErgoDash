4bdc4a6c45c74307ba17d1ca885acd4c47ee3b9f
Daehyeok Mun <daehyeok@gmail.com>
2022-07-05 23:09:35 -0700

    temp

--- firmware/code.py
+++ firmware/code.py
@@ -7,64 +7,19 @@
 import board
 import digitalio
 import supervisor
-import storage
 
-from kmk.kmk_keyboard import KMKKeyboard
-from kmk.keys import KC
-from kmk.scanners import DiodeOrientation
-from kmk.modules.split import Split, SplitSide, SplitType
+from kmk.modules.split import SplitSide
 
 from ergodash.ergodash import Ergodash
 
-def debug_print(*argv):
-    """print message to serial consol."""
-    if keyboard.debug_enabled:
-        print(*argv)
-
-
-def is_usb_connected():
-    """trickly way to check usb sorage and serial is enabled or not."""
-    try:
-        storage.remount("/", readonly=False)  # attempt to mount readwrite
-        storage.remount("/", readonly=True)  # attempt to mount readonly
-    except RuntimeError as _:
-        return True
-    return False
-
-
-
-keyboard = KMKKeyboard()
-keyboard.debug_enabled = is_usb_connected()
-keyboard.diode_orientation = DiodeOrientation.COL2ROW
-
-ergodash = Ergodash(keyboard.debug_enabled)
-keyboard.keymap = ergodash.keymap
-keyboard.col_pins = ergodash.col_pins
-keyboard.row_pins = ergodash.row_pins
-data_pin = board.GP0
-data_pin2 = board.GP1
-
-
-split = Split(
-    split_side=ergodash.split_side,
-    split_flip=False,
-    # Sets if this is to SplitSide.LEFT or SplitSide.RIGHT, or use EE hands
-    split_type=SplitType.UART,  # Defaults to UART
-    uart_interval=20,  # Sets the uarts delay. Lower numbers draw more power
-    data_pin=data_pin,
-    data_pin2=data_pin2,
-    debug_enabled=keyboard.debug_enabled,
-    # If you want the right to be the target, change this to false
-)
-
-#keyboard.modules.append(split)
+ergodash = Ergodash()
 
 if __name__ == "__main__":
     try:
         led = digitalio.DigitalInOut(board.LED)
         led.direction = digitalio.Direction.OUTPUT
         led.value = True
-        keyboard.go()
+        ergodash.go()
     except Exception as inst:  # pylint: disable=W0703
-        debug_print(inst)
+        ergodash.log(inst)
         supervisor.reload()
--- firmware/ergodash/ergodash.py
+++ firmware/ergodash/ergodash.py
@@ -2,30 +2,48 @@
 
 import board
 import digitalio
+import storage
+
+from kmk.kmk_keyboard import KMKKeyboard as _KMKKeyboard
+from kmk.scanners import DiodeOrientation
+
 from ergodash.layout import Keymap
 from ergodash.pins import Pinconfig
 
-from kmk.modules.split import SplitSide
+from kmk.modules.split import Split, SplitSide, SplitType
 
-class Ergodash:
+def _usb_mounted():
+    """trickly way to check usb sorage and serial is enabled or not."""
+    try:
+        storage.remount("/", readonly=False)  # attempt to mount readwrite
+        storage.remount("/", readonly=True)  # attempt to mount readonly
+    except RuntimeError as _:
+        return True
+    return False
+
+class Ergodash(_KMKKeyboard):
 
     split_side = SplitSide.LEFT
-    handress_pin= digitalio.DigitalInOut(board.GP5)
-    debug_enabled = False
 
-    def _log(self,*argv):
+    debug_enabled = _usb_mounted()
+    diode_orientation = DiodeOrientation.COL2ROW
+    keymap=Keymap
+    coord_mapping = list(range(70))
+
+    _tx_pin=board.GP0
+    _rx_pin=board.GP1
+    _handress_pin= digitalio.DigitalInOut(board.GP5)
+
+    def log(self,*argv):
         """print message to serial consol."""
         if self.debug_enabled:
             print(*argv)
 
-    def __init__(self,debug_enabled=False, split_side=None):
-        self.debug_enabled = debug_enabled
-        self.keymap=Keymap
-
+    def __init__(self, split_side=None):
         if split_side is not None:
             self.split_side = split_side
         else:
-            self.split_side = SplitSide.LEFT if self.handress_pin.value else SplitSide.RIGHT
+            self.split_side = SplitSide.LEFT if self._handress_pin.value else SplitSide.RIGHT
 
         if(self.split_side == SplitSide.LEFT):
             self.col_pins = Pinconfig.LEFT_COL_PINS
@@ -34,4 +52,30 @@ class Ergodash:
             self.col_pins = Pinconfig.RIGHT_COL_PINS
             self.row_pins = Pinconfig.RIGHT_ROW_PINS
 
-        self._log(f"Ergodox Init: {self.split_side}")
+        self.is_host = self.split_side == SplitSide.RIGHT
+        self._init_split_module()
+        self.log(f"Ergodox Init: {self.split_side}")
+
+    def _split_target_left(self):
+        if self.split_side == SplitSide.LEFT:
+            return self.is_host
+
+        return not self.is_host
+
+    def _init_split_module(self):
+        data_pin, data_pin2 = self._tx_pin, self._rx_pin
+        if(self.is_host):
+            data_pin, data_pin2 = self._rx_pin, self._tx_pin
+
+        split = Split(
+            split_side=self.split_side,
+            split_flip=False,
+            # Sets if this is to
+            split_target_left=self._split_target_left(),
+            split_type=SplitType.UART,# Defaults to
+            data_pin=data_pin,
+            data_pin2=data_pin2,
+            debug_enabled=self.debug_enabled,
+            # If you want the right to be the target, change this to false
+        )
+        self.modules.append(split)
